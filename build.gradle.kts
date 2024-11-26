b7bd7befbad9200d67fd30c005eaf18f78bf7a19
Xi Qi <xqi@thoughtworks.com>
2024-11-26 15:14:31 +0800

    chore: add jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	id("org.springframework.boot") version "3.4.0"
 	id("io.spring.dependency-management") version "1.1.6"
 //	id("org.springframework.cloud.contract") version "4.1.4"
+	jacoco
 }
 
 group = "com.example"
@@ -56,4 +57,19 @@ dependencyManagement {
 
 tasks.withType<Test>().configureEach {
 	useJUnitPlatform()
+	finalizedBy(tasks.named("jacocoTestReport"))
+}
+
+tasks.named<JacocoCoverageVerification>("jacocoTestCoverageVerification") {
+	violationRules {
+		rule {
+			limit {
+				minimum = "0.7".toBigDecimal()
+			}
+		}
+	}
+}
+
+tasks.named("check") {
+	dependsOn(tasks.named("jacocoTestCoverageVerification"))
 }
